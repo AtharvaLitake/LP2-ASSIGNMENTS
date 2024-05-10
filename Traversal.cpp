12f911c24303c650f5c38fa7f23fb0982cd7c60c
AtharvaLitake <atharvalitake24@gmail.com>
2024-05-10 19:01:05 +0530

    Updated BFS,DFS

--- Traversal.cpp
+++ Traversal.cpp
@@ -1,288 +1,126 @@
 #include<bits/stdc++.h>
 using namespace std;
-void bfs_rec(vector<int>vec[],vector<int> &visit, queue<int> & q,vector<int> & ans)
+class Graph
 {
-    if(q.empty())
+    public:
+    map<string,vector<string>>adj;
+    
+    //to add edge
+    void addedge(string computer1,string computer2)
     {
-        return ;
+        adj[computer1].push_back(computer2);
+        adj[computer2].push_back(computer1);
     }
-    int ele=q.front();
-    q.pop();
-    ans.push_back(ele);
-    vector<int>a=vec[ele];
-    for(int i=0;i<a.size();i++)
+    void printgraph()
     {
-        if(!visit[a[i]])
+        cout<<endl;
+        cout<<"PRINTING GRAPH--"<<endl;
+        for(auto computer:adj)
         {
-            visit[a[i]]=1;
-            cout<<"Visiting "<<a[i]<<" Vertex and inserting it into queue\n";
-            q.push(a[i]);
+            cout<<computer.first<<" --> ";
+            for(auto it: computer.second)
+            {
+                cout<<it<<" ";
+            }
+            cout<<endl;
         }
     }
-    bfs_rec(vec,visit,q,ans);
-}
-void BFS(vector<int>vec[],int n)
-{
-    vector<int>visit(n+1,0);
-    queue<int>q;
-    vector<int>ans;
-    for(int i=1;i<=n;i++)
+    void BFS_Traversal(string src)
     {
-        if(visit[i]==0)
+        vector<pair<string,int>>traversal;
+        set<string>visited;
+        queue<pair<string,int>>q;
+        visited.insert(src);
+        q.push({src,0});
+        BFS(q,visited,traversal);
+        printTraversal("BFS",traversal);
+    }
+    void BFS(queue<pair<string,int>>&q,set<string>&visited,vector<pair<string,int>>&traversal)
+    {
+        if(q.empty())
         {
-
-            visit[i]=1;
-            cout<<"Visiting "<<i<<" Vertex and inserting it into stack\n";
-            q.push(i);
-             bfs_rec(vec,visit,q,ans);
+            return;
+        }
+        pair<string,int>computer=q.front();
+        q.pop();
+        traversal.push_back(computer);
+        for(auto neighbour:adj[computer.first])
+        {
+            if(visited.find(neighbour)==visited.end())
+            {
+                visited.insert(computer.first);
+                q.push({neighbour,computer.second+1});
+            }
+        }
+        BFS(q,visited,traversal);
+    }
+    void DFS_Traversal(string src)
+    {
+        set<string>visited;
+        vector<pair<string,int>>traversal;
+        DFS(src,0,visited,traversal);
+        printTraversal("DFS",traversal);
+    }
+    void DFS(string src,int level,set<string> &visited,vector<pair<string,int>> &traversal)
+    {
+        traversal.push_back({src,level});
+        visited.insert(src);
+        for(auto neighbour:adj[src])
+        {
+            if(visited.find(neighbour)==visited.end())
+            {
+                DFS(neighbour,level+1,visited,traversal);
+            }
         }
     }
-    cout<<"\nBFS Traversal is : ";
-    for(int i=0;i<ans.size();i++)
+    void printTraversal(string name,vector<pair<string,int>>&traversal)
     {
-        cout<<ans[i]<<" ";
-    }
-    cout<<"\n";
-
-}
-void bfs_rec_search(vector<int>vec[],vector<int> &visit, queue<int> & q,vector<int> & ans,int & find,int target)
-{
-    if(q.empty())
-    {
-        return ;
-    }
-    int ele=q.front();
-    q.pop();
-    ans.push_back(ele);
-    if(ele==target)
-    {
-        find=1;
-        return ;
-    }
-
-    vector<int>a=vec[ele];
-    for(int i=0;i<a.size();i++)
-    {
-        if(!visit[a[i]])
+        cout<<name<<" Traversal --> ";
+        for(auto computer:traversal)
         {
-            visit[a[i]]=1;
-
-            q.push(a[i]);
-        }
-        if(find==1)
-        {
-            break;
+            cout<<" ( "<<computer.first<<", "<<computer.second<<" ) ";
         }
     }
-    if(find!=1)
-    {
-        bfs_rec_search(vec,visit,q,ans,find,target);
-    }
-}
-void BFS_Search(vector<int>vec[],int n,int target)
-{
-     vector<int>visit(n+1,0);
-    queue<int>q;
-    vector<int>ans;
-    int find=0;
-    for(int i=1;i<=n;i++)
-    {
-        if(visit[i]==0)
-        {
-            visit[i]=1;
-            q.push(i);
-            bfs_rec_search(vec,visit,q,ans,find,target);
-        }
-        if(find==1)
-        {
-            break;
-        }
-    }
-    if(find==1)
-    {
-        cout<<"\nVertex Found \n";
-        cout<<"\nBFS Traversal for vertex "<< target <<" is : ";
-        for(int i=0;i<ans.size();i++)
-        {
-            cout<<ans[i]<<" ";
-        }
-        cout<<"\n";
-    }
-    else
-    {
-        cout<<"\nVertex Not Found \n";
-    }
-
-}
-void dfs_rec(vector<int>vec[],vector<int> &visit,stack<int>st,vector<int> &ans)
-{
-    int n=st.top();
-    st.pop();
-     vector<int>a=vec[n];
-     ans.push_back(n);
-    // cout<<n<<" ";
-     for(int i=0;i<a.size();i++)
-     {
-         int ele=a[i];
-         if(visit[a[i]]==0)
-         {
-             visit[a[i]]=1;
-             cout<<"Visiting "<<a[i]<<" Vertex and inserting it into stack\n";
-            st.push(a[i]);
-             dfs_rec(vec,visit,st,ans);
-         }
-     }
-}
-void dfs_rec_search(vector<int>vec[],vector<int> &visit,stack<int>st,vector<int> &ans,int & find,int target)
- {
-      int n=st.top();
-    st.pop();
-     vector<int>a=vec[n];
-     ans.push_back(n);
-     if(n==target)
-     {
-         find=1;
-         return;
-     }
-
-     for(int i=0;i<a.size();i++)
-     {
-         int ele=a[i];
-         if(visit[a[i]]==0)
-         {
-             visit[a[i]]=1;
-
-             st.push(a[i]);
-             dfs_rec_search(vec,visit,st,ans,find,target);
-             if(find==1)
-             {
-                 break;
-             }
-         }
-     }
- }
-void DFS_Search(vector<int>vec[],int n,int target)
-{
-    vector<int>ans;
-    vector<int>visit(n+1,0);
-    stack<int>st;
-      int find=0;
-    for(int i=1;i<=n;i++)
-    {
-        if(visit[i]==0)
-        {
-            visit[i]=1;
-
-            st.push(i);
-            dfs_rec_search(vec,visit,st,ans,find,target);
-        }
-        if(find==1)
-        {
-            break;
-        }
-    }
-     if(find==1)
-    {
-        cout<<"\nVertex Found \n";
-        cout<<"\nDFS Traversal for vertex is : ";
-        for(int i=0;i<ans.size();i++)
-        {
-            cout<<ans[i]<<" ";
-        }
-        cout<<"\n";
-    }
-    else
-    {
-        cout<<"\nVertex Not Found \n";
-    }
-}
-void DFS(vector<int>vec[],int n)
-{
-    vector<int>ans;
-    vector<int>visit(n+1,0);
-    stack<int>st;
-    for(int i=1;i<=n;i++)
-    {
-        if(visit[i]==0)
-        {
-            visit[i]=1;
-            cout<<"Visiting "<<i<<" Vertex and inserting it into stack\n";
-            st.push(i);
-            dfs_rec(vec,visit,st,ans);
-        }
-    }
-
-     cout<<"\nDFS Traversal is : ";
-    for(int i=0;i<ans.size();i++)
-    {
-        cout<<ans[i]<<" ";
-    }
-    cout<<"\n";
-
-
-}
-
+};
 int main()
 {
-    int n,edge,u,v;
-    cout<<"Enter Number of Vertices : ";
-    cin>>n;
-    vector<int>vec[n+1];
-    cout<<"Enter Number of Edges : ";
-    cin>>edge;
-    for(int i=0;i<edge;i++)
+    Graph g;
+    string computer1,computer2;
+    int a;
+    cout<<"1.Adding Edge\n2.BFS\n3.DFS\n4.Printing\n5.END\n";
+    while(true)
     {
-        cout<<"Enter source index : ";
-        cin>>u;
-        cout<<"Enter destination vertex : ";
-        cin>>v;
-        vec[u].push_back(v);
-        vec[v].push_back(u);
-    }
-
-    cout<<"\nGraph\n";
-
-    for(int i=1;i<n+1;i++)
-    {
-        cout<<i<<" -> ";
-        vector<int>a=vec[i];
-        for(int j=0;j<vec[i].size();j++)
-         {
-             cout<<a[j]<<" ";
-         }
-        cout<<"\n";
-    }
-
-    cout<<"\n\nBFS Treversal \n\n";
-    BFS(vec,n);
-
-    cout<<"\n\nDFS Treversal \n\n";
-    DFS(vec,n);
-
-    while(1)
-    {
-        cout<<"\n\nEnter the node to search : ";
-        int node;
-        cin>>node;
-        cout<<"\n1. BFS";
-        cout<<"\n 2.DFS";
-        cout<<"\n 3. Exit";
-        cout<<"\nChoice : ";
-        int ch;
-        cin>>ch;
-        switch(ch)
-        {
-            case 0:
-                exit(1);
-                break;
-            case 1:
-                BFS_Search(vec,n,node);
-                break;
-            case 2:
-                DFS_Search(vec,n,node);
-                break;
-            default:
-            cout<<"Invalid choice\n";
-        }
+        cout<<"Enter Value-->"<<endl;
+        cin>>a;
+     if(a==1)
+     {
+         cout<<"Enter Computer 1-->"<<endl;
+         cin>>computer1;
+         cout<<"Enter Computer 2-->"<<endl;
+         cin>>computer2;
+         g.addedge(computer1,computer2);
+     }
+     if(a==2)
+     {
+         cout<<"Enter Source"<<endl;
+         string src;
+         cin>>src;
+         g.BFS_Traversal(src);
+     }
+     if(a==3)
+     {
+         cout<<"Enter Source"<<endl;
+         string src;
+         cin>>src;
+         g.DFS_Traversal(src);
+     }
+     if(a==4)
+     {
+         g.printgraph();
+     }
+     if(a==5)
+     {
+         break;
+     }
     }
 }
